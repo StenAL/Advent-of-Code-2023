a2ae00af8d248ae328766e6e27a48b99412d9044
Sten Laane <stenarthur.laane@mil.ee>
2023-12-01 14:53:53 +0200

    Partial

--- fetchInput.sh
+++ fetchInput.sh
@@ -9,7 +9,7 @@ fi
 
 inputFile="src/input/day$day.txt"
 if [[ ! -f $inputFile ]]; then
-  curl -sS --cookie "session=$(cat cookie.txt)" "https://adventofcode.com/2022/day/$day/input" -o $inputFile
+  curl -sS --cookie "session=$(cat cookie.txt)" "https://adventofcode.com/2023/day/$day/input" -o $inputFile
   echo "Fetched $inputFile"
   git add $inputFile
 else
