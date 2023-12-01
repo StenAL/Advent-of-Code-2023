8eb01defcd07eb9632960e9899a80f0e692a9d7b
Sten Laane <stenarthur.laane@mil.ee>
2023-12-01 11:09:30 +0200

    Add boilerplate files
    Copied from last year.

---
+++ fetchInput.sh
@@ -0,0 +1,27 @@
+set -euo pipefail
+
+day=$1
+if [[ -z "$1" ]]; then
+  echo "missing argument for day"
+  echo "usage: ./fetchInput DAY"
+  exit 1;
+fi
+
+inputFile="src/input/day$day.txt"
+if [[ ! -f $inputFile ]]; then
+  curl -sS --cookie "session=$(cat cookie.txt)" "https://adventofcode.com/2022/day/$day/input" -o $inputFile
+  echo "Fetched $inputFile"
+  git add $inputFile
+else
+  echo "$inputFile already exists"
+fi
+
+sourceFile="src/day$day.py"
+if [[ ! -f $sourceFile ]]; then
+  cp src/skeleton.py src/day$day.py
+  sed -i "s/'REPLACE_ME'/$day/g" src/day$day.py
+  echo "Created $sourceFile"
+  git add $sourceFile
+else
+  echo "$sourceFile already exists"
+fi
---
+++ src/skeleton.py
@@ -0,0 +1,21 @@
+from util import *
+from collections import *
+import copy
+from functools import reduce
+from math import prod
+
+day = 'REPLACE_ME'
+
+
+def task1():
+    data = get_input_for_day(day)
+    # data = get_input_for_file("test")
+
+
+def task2():
+    data = get_input_for_day(day)
+    # data = get_input_for_file("test")
+
+
+task1()
+# task2()
